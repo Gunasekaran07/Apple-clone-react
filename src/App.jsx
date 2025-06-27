d66c99db807d75c4e6d1d7194fa29ac09a834f8c
Gunasekaran07 <bgunasekaran872001@gmail.com>
2025-06-27 13:12:08 +0530

    uploaded all files

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Hero from './components/Hero2'
+import Hero2 from './components/Hero2'
+import Navbar from './components/Navbar'
+import Product from './components/Product'
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <Header />
+      <Hero />
+      <Hero2 />      
+      <Product />
+      <Footer />      
+    </>
+  )
+}
+
+export default App
